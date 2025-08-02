9426da2e0de6266cb20ce3dafb8692c4b8027075
EggsF0rAll <leonardo.rosas@uteca.edu.mx>
2025-08-01 19:07:07 -0600

    Avance pre examen
    Torre nueva
    Correccion de errores

--- Assets/Scripts/Rotate.cs
+++ Assets/Scripts/Rotate.cs
@@ -5,7 +5,7 @@ public class Rotate : MonoBehaviour
     [SerializeField]
     private Vector3 _rotationspeed = new Vector3(0, 10f, 0);
 
-    void Update()
+    void LateUpdate()
     {
         transform.Rotate(_rotationspeed * Time.deltaTime, Space.Self);
     }
